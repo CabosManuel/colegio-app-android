802c2c1166bec72ef13fdf8ad6e0436f6b7f402c
CabosManuel <cabos.apolaya@gmail.com>
2020-12-15 00:08:19 -0500

    Nuevo proyecto

---
+++ NuevoProyecto/MPB/app/src/main/java/pe/mariaparadodebellido/ConsultarNotas.java
@@ -0,0 +1,14 @@
+package pe.mariaparadodebellido;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultarNotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_notas);
+    }
+}

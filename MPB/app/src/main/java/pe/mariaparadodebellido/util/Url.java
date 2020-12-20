a317e0665bcea29066dbf11ca5adfd5dddf4fbd8
CabosManuel <cabos.apolaya@gmail.com>
2020-12-19 22:50:53 -0500

    Clase util "Url", para almacenar IPs y puertos

---
+++ MPB/app/src/main/java/pe/mariaparadodebellido/util/Url.java
@@ -0,0 +1,6 @@
+package pe.mariaparadodebellido.util;
+
+public class Url {
+    public static final String IP ="192.168.0.27"; // CABOS = 192.168.0.27
+    public static final String PUERTO = "8085";
+}

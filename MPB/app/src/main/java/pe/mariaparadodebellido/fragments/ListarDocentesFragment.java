85e38363d304ff1dee7c574706ae53fd13402216
CabosManuel <cabos.apolaya@gmail.com>
2021-01-12 15:58:20 -0500

    Listar docentes (base)

---
+++ MPB/app/src/main/java/pe/mariaparadodebellido/fragments/ListarDocentesFragment.java
@@ -0,0 +1,24 @@
+package pe.mariaparadodebellido.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pe.mariaparadodebellido.R;
+
+public class ListarDocentesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View viewFragment = inflater.inflate(R.layout.fragment_listar_docentes, container, false);
+
+        // TODO EL CODIGO
+
+        return viewFragment;
+    }
+}
